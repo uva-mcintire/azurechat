0c87a5602aac5da76540eb449fd236af609db2fd
robbinsjd4 <109884511+robbinsjd4@users.noreply.github.com>
2024-04-15 12:29:45 -0400

    removed login card

--- src/features/auth-page/login.tsx
+++ src/features/auth-page/login.tsx
@@ -18,32 +18,20 @@ interface LoginProps {
 
 export const LogIn: FC<LoginProps> = (props) => {
   return (
-    <div className="flex items-center justify-center w-full"
+    <div className="flex flex-col items-center justify-center w-full h-screen"
      style={{
-       minHeight: '100vh',
-       minWidth: '100vw',
-       margin: '0',
-       padding: '0',
-       background: 'linear-gradient(to bottom, hsl(225, 35.71%, 21.96%), hsl(225, 23.68%, 29.8%))'
+       background: 'linear-gradient(to bottom, hsl(225, 35.71%, 21.96%), hsl(225, 23.68%, 29.8%)) !important'
      }}>
-      <Card className="bg-transparent border-none shadow-none">
-        <CardHeader className="gap-2">
-          <CardTitle className="text-2xl">
-            <img src="comm-ai.png" alt="McINTIRE{AI}" style={{ width: '529px', height: '60px' }} />
-          </CardTitle>
-          <CardDescription>
-            * BETA *
-          </CardDescription>
-        </CardHeader>
-        <CardContent className="grid gap-4">
-          <Button onClick={() => signIn("azure-ad")}>Login with UVA Credentials</Button>
-          {props.isDevMode ? (
-            <Button onClick={() => signIn("localdev")}>
-              Basic Auth (DEV ONLY)
-            </Button>
-          ) : null}
-        </CardContent>
-      </Card>
+      <img src="comm-ai.png" alt="McINTIRE{AI}" style={{ width: '529px', height: '60px', marginBottom: '20px' }} />
+      <p className="text-xl text-white mb-8">* BETA *</p>
+      <Button className="bg-primary text-white py-2 px-4 rounded" onClick={() => signIn("azure-ad")}>
+        Login with UVA Credentials
+      </Button>
+      {props.isDevMode && (
+        <Button className="bg-secondary text-white py-2 px-4 rounded mt-2" onClick={() => signIn("localdev")}>
+          Basic Auth (DEV ONLY)
+        </Button>
+      )}
     </div>
   );
 };
