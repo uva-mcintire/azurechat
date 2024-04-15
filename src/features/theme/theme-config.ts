c9486072a2816694ae499b1f55917f30448a778e
robbinsjd4 <109884511+robbinsjd4@users.noreply.github.com>
2024-04-15 10:46:54 -0400

    update theme

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -1,5 +1,5 @@
-export const AI_NAME = "McIntireAI";
-export const AI_DESCRIPTION = "McIntireAI is a friendly AI assistant.";
+export const AI_NAME = "McIntire{AI}";
+export const AI_DESCRIPTION = "McIntire{AI} is a friendly AI assistant.";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
 export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
