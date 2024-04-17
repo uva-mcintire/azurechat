6879dac3bba6d7a134c4d8165cf1e470b6e8584a
robbinsjd4 <109884511+robbinsjd4@users.noreply.github.com>
2024-04-17 15:34:07 -0400

    Update theme-config.ts

--- src/features/theme/theme-config.ts
+++ src/features/theme/theme-config.ts
@@ -2,9 +2,10 @@ export const AI_NAME = "McINTIRE{AI}";
 export const AI_DESCRIPTION = "McINTIRE{AI} is a friendly AI assistant. Use the pre-installed persona's or create your own.";
 export const CHAT_DEFAULT_PERSONA = AI_NAME + " default";
 
-export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly ${AI_NAME} AI assistant. You must always return in markdown format.
+export const CHAT_DEFAULT_SYSTEM_PROMPT = `You are a friendly AI assistant. You must always return in markdown format.
 
 You have access to the following functions:
-1. create_img: You must only use the function create_img if the user asks you to create an image.`;
+1. create_img: You must only use the function create_img if the user asks you to create an image.
+2. BingSearch: Utilize BingSearch to find up-to-date information and provide responses based on current events and topics.`;
 
 export const NEW_CHAT_NAME = "New chat";
